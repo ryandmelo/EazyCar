4f944b19109e8e84a73ce7007f9192494f91fbbb
unknown <ryanmelo2325@gmail.com>
2024-10-21 00:19:09 -0300

    Alteraçoes de configuraçoes

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,7 @@
-
 from django.contrib import admin
 from django.urls import path
 
 from todos.views import home
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',home)
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", home)]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home (request):
-    return render(request= "todos/home.html")
 
-# Create your views here.
+def home(request):
+    return render(request="todos/home.html")
